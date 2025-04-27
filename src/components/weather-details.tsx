acf658e56ad25a7f8125cdcc1f06d6bd3e4d3b9b
sumanth <mrsumanth1947@gmail.com>
2025-04-27 18:42:46 +0530

    weather deatils

--- src/components/weather-details.tsx
+++ src/components/weather-details.tsx
@@ -1,11 +1,75 @@
+import { Compass, Gauge, Sunrise, Sunset } from "lucide-react";
 import type { WeatherData } from "../api/types";
+import { format } from "date-fns";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 
 interface WeatherDetailsProps {
   data: WeatherData;
 }
 
 const WeatherDetails = ({ data }: WeatherDetailsProps) => {
-  return <div>cdd</div>;
+  const { wind, main, sys } = data;
+
+  const formatTime = (timestamp: number) => {
+    return format(new Date(timestamp * 1000), "h:mm a");
+  };
+
+  const getWindDirection = (degree: number) => {
+    const direction = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
+
+    const index = Math.round(((degree %= 360) < 0 ? degree + 360 : degree) / 45) % 8;
+    return direction[index];
+  };
+
+  const details = [
+    {
+      title: "Sunrise",
+      value: formatTime(sys.sunrise),
+      icon: Sunrise,
+      color: "text-orange-500",
+    },
+    {
+      title: "Sunset",
+      value: formatTime(sys.sunset),
+      icon: Sunset,
+      color: "text-blue-500",
+    },
+    {
+      title: "Wind Direction",
+      value: `${getWindDirection(wind.deg)} (${wind.deg})°`,
+      icon: Compass,
+      color: "text-green-500",
+    },
+    {
+      title: "Pressure",
+      value: `${main.pressure} hPa`,
+      icon: Gauge,
+      color: "text-purple-500 ",
+    },
+  ];
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Weather Deatils</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="grid md:grid-cols-2 gap-6">
+          {details.map((ele) => {
+            return (
+              <div key={ele.title} className="flex items-center gap-3 rounded-lg border p-4">
+                <ele.icon className={`h-5 w-5 ${ele.color}`} />
+                <div>
+                  <p className="text-sm font-medium leading-none">{ele.title}</p>
+                  <p className="text-sm font-medium text-muted-foreground">{ele.value}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default WeatherDetails;
